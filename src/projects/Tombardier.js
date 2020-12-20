c2163586028c451d17e8357219bb9a0921ef40d8
jdhofmann <justindhofmann@gmail.com>
2020-12-20 13:16:46 -0500

    more blue stuff

--- src/projects/Tombardier.js
+++ src/projects/Tombardier.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 export const Tombardier = () => {
     return (
-        <div>
+        <div className="main content">
             <h2>Tombardier</h2>
+            <p>Web portfolio building made simple</p>
+            <p>Tombardier comes out of the box ready to provide users with their own personal portfolio site. With an inline editable UI, users can edit content right in their browsers.</p>
+            <h3>Images</h3>
+            <div className="image-container">
+            </div>
+            <div>
+                <h3>Links</h3>
+                <a href="https://github.com/JDHofmann/tombardier-frontend-dev">Frontend Repository</a>
+                <a href="https://github.com/JDHofmann/tombardier-bakendend-dev">Backend Repository</a>
+            </div>
         </div>
     )
 }
