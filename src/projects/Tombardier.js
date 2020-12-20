a58acac830b656ad93adfddb5055dfa60a4160c6
jdhofmann <justindhofmann@gmail.com>
2020-12-20 18:07:52 -0500

    image slider added

--- src/projects/Tombardier.js
+++ src/projects/Tombardier.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import Slider from './Slider'
+import tom01 from '../images/tombardier01.png'
+import zen from '..//images/zen.png'
 
 export const Tombardier = () => {
+
+    const images = [tom01, zen];
+
     return (
         <div className="main content">
             <h2>Tombardier</h2>
             <p>Web portfolio building made simple</p>
             <p>Tombardier comes out of the box ready to provide users with their own personal portfolio site. With an inline editable UI, users can edit content right in their browsers.</p>
-            <h3>Images</h3>
-            <div className="image-container">
-            </div>
+            <Slider 
+                images={images}
+            />
             <div>
                 <h3>Links</h3>
                 <a href="https://github.com/JDHofmann/tombardier-frontend-dev">Frontend Repository</a>
