43aed1962a6b7c978a21dd12165ff1f7e884cb43
jdhofmann <justindhofmann@gmail.com>
2020-12-20 13:44:23 -0500

    navy color scheme

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         <>
         <a href="#main">skip to content</a>
         <header className="dark">
-            <div className="outter titles">
+            <div className="titles">
                 <h1>JD Brewer-Hofmann</h1>
                 <p className="subtitle">Web Devoloper // All around good person</p>
             </div>
