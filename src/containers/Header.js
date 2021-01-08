816fabd2af023f7894f311bc9289ef80da464447
jdhofmann <justindhofmann@gmail.com>
2021-01-07 21:14:43 -0500

    h1 fix

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -2,8 +2,13 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Header = () => {
+    // console.log(window.location.pathname)
     return (
         <li className="li-top-grid grid">
+            {window.location.pathname==="/" ? <div className="pho-title-wrapper">
+                <h1>JD Brewer-Hofmann</h1>
+            </div>
+            : 
                 <NavLink
                     title="home page"
                     to="/"
@@ -11,11 +16,13 @@ const Header = () => {
                 >
                     <h1>JD Brewer-Hofmann</h1>
                 </NavLink>
+            }
                 <span className="line"></span>
                 <span className="box br-tr-br"></span>
                 <p className="subtitle"
                 >Web Developer</p>                
             </li>
+
     )
 }
 export default Header
