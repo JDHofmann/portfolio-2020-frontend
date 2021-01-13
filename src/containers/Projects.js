0c516d09df8e2f097aa3b5046eb03fd8e2e08e59
jdhofmann <justindhofmann@gmail.com>
2021-01-12 20:42:25 -0500

    added store for projects

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -1,31 +1,40 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-
+import { projects } from '../store.js'
 
 const Projects = () => {
+
+    const renderProjectLinks = () => {
+        return (
+            // projectLinks.map()
+            console.log(projects)
+        )
+    }
+
     return (
             <li>
                 <div className="li-project-grid grid">
                     <h2>Featured Projects</h2>
                     <span className="line"></span>
                     <span className="box bt-light br-tr"></span>
-                    <NavLink
+                    {renderProjectLinks()}
+                    {/* <NavLink
                         to="/tombardier"
                         className="sub-item pj"
                     ><h3>Tombardier</h3>
-                    </NavLink>
+                    </NavLink> */}
                     <span className="box box-item-1-2"></span>
-                    <NavLink 
+                    {/* <NavLink 
                         to="/bestbey"
                         className="sub-item sub-item-2 pj"
                     ><h3>Bestbey</h3>
-                    </NavLink>
+                    </NavLink> */}
                     <span className="box box-item-end br-br"></span>
-                    <NavLink 
+                    {/* <NavLink 
                         to="/starwars"
                         className="sub-item sub-item-3 pj"
                     ><h3>Star Wars Quiz</h3>
-                    </NavLink>
+                    </NavLink> */}
                 </div>
             </li>
     )
---
+++ src/store.js
@@ -0,0 +1,43 @@
+export const projects = [
+    {
+        path: "/tombardier",
+        title : "Tombardier",
+        subtitle: "Portfolio Building Made Simple",
+        description: "Tombardier comes out of the box ready to provide users with their own personal portfolio site. With an inline editable UI, users can edit content right in their browsers. Built with React, Redux, Ruby on Rails, Active Storage, and AWS cloud storage.",
+        links : [
+            {linkUrl: "https://tombardier.netlify.app/",
+            linkText: "Live Demo"},
+            {linkUrl: "https://github.com/JDHofmann/tombardier-frontend-dev",
+            linkText: "Frontend Repo"},
+            {linkUrl: "https://github.com/JDHofmann/tombardier-backend-dev",
+            linkText: "Backend Repo"}
+        ]
+    },
+    {
+        path: "/bestbey",
+        title : "Bestbey",
+        subtitle: "E-commerce electronics",
+        description: "Bestbey is an e-commerce site utilizing a retail product database, a full user interface, and a customized shopping cart. Built with React and a Ruby on Rails API.",
+        links : [
+            {linkUrl: "https://bestbey.netlify.app/",
+            linkText: "Live Demo"},
+            {linkUrl: "https://github.com/JDHofmann/bestbey-frontend",
+            linkText: "Frontend Repo"},
+            {linkUrl: "https://github.com/JDHofmann/bestbey-backend-v2.1",
+            linkText: "Backend Repo"}
+        ]
+    },
+    {
+        path: "/starwars",
+        title : "Star Wars Quiz",
+        subtitle: "Movie Quiz",
+        description: "A trivia game that tests your knowledge of Star Wars. The quiz is 10 questions where you can find out if you have what it takes to become a Jedi Master. Built with HTML5, CSS, Javascript, and Jquery.",
+        links : [
+            {linkUrl: "https://jdhofmann.github.io/Star-Wars-Quiz/",
+            linkText: "Live Demo"},
+            {linkUrl: "https://github.com/JDHofmann/Star-Wars-Quiz",
+            linkText: "Github Repo"}
+        ]
+    }
+
+]
