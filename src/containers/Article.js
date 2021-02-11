9a73a79697fb06146b93f7859b52ee74661be881
jdhofmann <justindhofmann@gmail.com>
2021-02-11 11:27:07 -0500

    grey matter

--- src/containers/Article.js
+++ src/containers/Article.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-
+import matter from 'gray-matter'
 
 export default function Article(){
 
     const [ article, setArticle ] = useState(null)
+    const [ title, setTitle ] = useState(null)
 
     useEffect( () => {
         fetchData()
@@ -12,21 +13,23 @@ export default function Article(){
       const fetchData = async() => {
         const result = await fetch(`https://dev.to/api/articles/589849`)
         const data = await result.json()
-        setArticle(data)
+        setTitle(data.title)
+        setArticle(matter(data.body_markdown))
       }
 
     const renderArticle = () => {
         return article === null ? <p>loading...</p> :
         <>
-        <h1>{article.title}</h1>
-        {`${article.body_markdown}`}
+        <h1>{title}</h1>
+        <div>{article.content}</div>
+        
         </>
     }
 
     return(
         <div>
             {renderArticle()}
-            {console.log(article)}
+            {/* {console.log(article)} */}
         </div>
     )
 }
